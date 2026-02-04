4322f732f81df7bb08b8e2ffe91cd9102623f783
Ana Maria Brito Souza <160272076+soouzaana@users.noreply.github.com>
2026-02-04 15:30:48 -0400

    fix: fix index archive

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <?php wp_head(); ?>
-</head>
-<body>
+<?php
+get_header();
+?>
 
-<?php wp_footer(); ?>
-</body>
-</html>
+<main>
+  <h1>INDEX FALLBACK</h1>
+
+  <?php
+  if (have_posts()) :
+    while (have_posts()) : the_post();
+      the_title('<h2>', '</h2>');
+      the_content();
+    endwhile;
+  else :
+    echo '<p>Sem conteúdo</p>';
+  endif;
+  ?>
+</main>
+
+<?php
+get_footer();
